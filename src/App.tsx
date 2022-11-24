e4098ac8135e2a3a1f110262c94f1bd2dc0be2ad
Quan Nguyen (Dave) <nvhoangquan1006@gmail.com>
2022-11-24 17:37:19 +0700

    first commit -setup things

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import EmojiPicker, { Theme, EmojiClickData } from "emoji-picker-react";
+import { useState } from "react";
+
+const copyToClipboard = (text: string) => navigator.clipboard.writeText(text);
 
 function App() {
+  const [currentEmojiData, setCurrentEmojiData] = useState<EmojiClickData>();
+  function onClick(emojiData: EmojiClickData, event: MouseEvent) {
+    setCurrentEmojiData(emojiData);
+    copyToClipboard(emojiData.emoji);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {currentEmojiData ? (
+          <div style={{ textAlign: "center", marginBottom: "16px" }}>
+            <span
+              style={{
+                color: "white",
+                fontSize: "22px",
+                marginRight: "8px",
+                fontWeight: "bold",
+              }}
+            >
+              Copied to clipboard!
+            </span>
+            <input
+              type="text"
+              style={{
+                fontSize: "24px",
+                width: "30px",
+                textAlign: "center",
+                borderRadius: "8px",
+                backgroundColor: "white",
+                border: "1px solid grey",
+                cursor: "pointer",
+              }}
+              value={currentEmojiData.emoji}
+              onClick={() => copyToClipboard(currentEmojiData.emoji)}
+              disabled
+            />
+          </div>
+        ) : (
+          <p
+            style={{
+              marginTop: 0,
+              color: "white",
+              fontSize: "20px",
+              textAlign: 'center',
+              fontWeight: 'bold',
+              marginBottom: '12px'
+            }}
+          >
+            Click on emoji to copy.
+          </p>
+        )}
+
+        <div className="picker-container">
+          <EmojiPicker theme={Theme.LIGHT} onEmojiClick={onClick} />
+        </div>
+      </div>
     </div>
   );
 }
